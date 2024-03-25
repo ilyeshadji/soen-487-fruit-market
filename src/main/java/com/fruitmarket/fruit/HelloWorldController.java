a6cae6c1e35c85ac3c539772c5d2c627ce91d297
ilyeshadji <ilyes.hadji@live.fr>
2024-03-25 15:31:44 -0400

    running version

--- src/main/java/com/fruitmarket/fruit/HelloWorldController.java
+++ src/main/java/com/fruitmarket/fruit/HelloWorldController.java
@@ -5,9 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-
     @GetMapping(path = "/hello")
     public String helloWorld(){
-        return "Hello World!";
+        return "Hello World";
     }
 }
